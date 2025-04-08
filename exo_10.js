5fc2914a3c9316259905d0b137a8bb2a8be0083d
Giraud Sasha <giraudsasha@gmail.com>
2025-04-08 09:57:43 +0200

    changed line on exo 10

--- exo_10.js
+++ exo_10.js
@@ -1,18 +1,16 @@
 for (let i = 1; i <= 100; i++) {
     let output = i;
-    
-    // Vérification divisible 3 et 5
+    // divisible 3 et 5
     if (i % 3 === 0 && i % 5 === 0) {
         output = `${i} foobar`;
     } 
-    // Vérification divisible par 3
+    // divisible par 3
     else if (i % 3 === 0) {
         output = `${i} foo`;
     } 
-    // Vérification divisible par 5
+    // divisible par 5
     else if (i % 5 === 0) {
         output = `${i} bar`;
     }
-    
     console.log(output);  
 }
